285290a7f9dd8572f21471aee1807403d5e252a5
rezahsnz <rezahsnz@proton.me>
2025-12-21 16:27:21 +0330

    feat: - add peer to the DHt on AddExternalAddressOfPeer event chore: - bump libp2p to v0.56

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,7 @@
 #![doc = include_str!("../README.md")]
 
 use std::{
-    time::{
-        Duration
-    },
+    time::Duration,
     env,
 };
 
@@ -18,7 +16,6 @@ use tokio::time::interval;
 use tokio_stream::wrappers::IntervalStream;
 
 use libp2p::{
-    core::ConnectedPoint,
     identity,
     identify, 
     kad,
@@ -26,6 +23,7 @@ use libp2p::{
     swarm::{
         SwarmEvent
     },
+    multiaddr::Protocol,
 };
 
 use tracing_subscriber::EnvFilter;
@@ -71,30 +69,21 @@ async fn main() -> anyhow::Result<()> {
     };   
     println!("my peer id: `{:?}`", PeerId::from_public_key(&local_key.public()));    
 
-    let my_ip_addr = env::var("MY_IP_ADDR")
-        .context("`MY_IP_ADDR` environment variable does not exist.")?;     
-    let mut swarm = setup_swarm_for_bootnode(&local_key)?;    
-    swarm.listen_on(
-        format!(
-            "/ip4/{}/udp/20201/quic-v1",
-            my_ip_addr
-        )
-        .parse()?
-    )?;
-    swarm.listen_on(
-        format!(
-            "/ip4/{}/tcp/20201",
-            my_ip_addr
-        )
-        .parse()?
-    )?;
+    let mut swarm = setup_swarm_for_bootnode(&local_key)?;
+    swarm.listen_on("/ip4/0.0.0.0/tcp/20201".parse()?)?;
+    swarm.listen_on("/ip4/0.0.0.0/udp/20201/quic-v1".parse()?)?;
+    swarm.behaviour_mut()
+        .kademlia
+        .set_mode(
+            Some(kad::Mode::Server)
+        );    
 
     let mut timer_peer_discovery = IntervalStream::new(
-        interval(Duration::from_secs(60))
+        interval(Duration::from_secs(30))
     )
     .fuse();
     
-    println!("protocol names: {:#?}", swarm.behaviour_mut().kademlia.protocol_names());
+    println!("Protocol names: {:#?}", swarm.behaviour_mut().kademlia.protocol_names());
     loop {
         select! {
             // try to discover new peers
@@ -103,7 +92,13 @@ async fn main() -> anyhow::Result<()> {
                 println!("Searching for the closest peers to `{random_peer_id}`");
                 swarm.behaviour_mut()
                     .kademlia
-                    .get_closest_peers(random_peer_id);                
+                    .get_closest_peers(random_peer_id);
+                for bucket in swarm.behaviour_mut().kademlia.kbuckets() {
+                    for entry in bucket.iter() {
+                        let peer = entry.node.key.preimage();
+                        println!("known peer: {}", peer);
+                    }
+                }
             },
 
             event = swarm.select_next_some() => match event {
@@ -117,33 +112,47 @@ async fn main() -> anyhow::Result<()> {
                     ..
                 } => {
                     println!(
-                        "A connection has been established to {}@{:?}",
+                        "A connection has been established to {} via {:?}",
                         peer_id,
                         endpoint
                     );
-                    let addr = match endpoint {
-                        ConnectedPoint::Dialer { address, .. } => {
-                            address
-                        },
-
-                        ConnectedPoint::Listener { send_back_addr, .. } => {
-                            send_back_addr
-                        }
-                    };
-                    swarm.behaviour_mut()
-                        .kademlia
-                        .add_address(&peer_id, addr);
                 },
+
 
                 SwarmEvent::Behaviour(BootNodeBehaviourEvent::Identify(identify::Event::Received {
                     peer_id,
-                    info
+                    info,
+                    ..
                 })) => {
                     println!(
-                        "Inbound identify event from {}: {:#?}",
+                        "Received identify from {}: {:#?}",
                         peer_id,
                         info
-                    );                                                       
+                    );                                                               
+                },
+
+                SwarmEvent::NewExternalAddrOfPeer {
+                    peer_id,
+                    address
+                } => {
+                    let is_public = address.iter()
+                        .filter_map(|c| 
+                            if let Protocol::Ip4(ip4_addr) = c {
+                                Some(ip4_addr)
+                            } else {
+                                None
+                            }
+                        )
+                        .all(|a| !a.is_private() && !a.is_loopback());
+                    if is_public {                        
+                        println!(
+                            "added public address of the peer to the DHT: {}",
+                            address
+                        );
+                        swarm.behaviour_mut()
+                            .kademlia
+                            .add_address(&peer_id, address);
+                    }                      
                 },
 
                 SwarmEvent::Behaviour(BootNodeBehaviourEvent::Kademlia(kad::Event::OutboundQueryProgressed {
